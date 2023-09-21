4d1f6e9baf6f041c0ab87d0bf613e7fc550d90f5
AngeloOliveiraTech <angelodeoliveira123@gmail.com>
2023-09-21 16:00:22 -0300

    TryBD6

--- _app/conexao.php
+++ _app/conexao.php
@@ -1,20 +1,17 @@
 <?php
 
-// try 
-//  {
-//      $dsn = 'mysql:dbname=TCC; host=netcase.mysql.database.azure.com; charset=utf8';
-//      $user = 'netcase';
-//     $password = 'Tcc@2023';
+try 
+ {
+      $dsn = 'mysql:dbname=TCC; host=netcase.mysql.database.azure.com; port=3306; charset=utf8';
+      $user = 'netcase';
+      $password = 'Tcc@2023';
     
-//      $dbh = new PDO($dsn, $user, $password);
+      $dbh = new PDO($dsn, $user, $password);
 
-//  }
-//  catch(PDOException $e)
-//  {
-//      echo "Erro na conexão: " . $e->getMessage();
-//      die("Não foi possível estabelecer a conexão com o banco de dados.");
-//  }
+  }
+  catch(PDOException $e)
+  {
+      echo "Erro na conexão: " . $e->getMessage();
+      die("Não foi possível estabelecer a conexão com o banco de dados.");
+  }
 
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "netcase.mysql.database.azure.com", "netcase", "Tcc@2023dugvdbs", "tcc", 3306, MYSQLI_CLIENT_SSL);
