d9947298fdfa383ab096218c31440b9a7f2f9fe3
AngeloOliveiraTech <angelodeoliveira123@gmail.com>
2023-09-21 11:25:07 -0300

    BDConfigurado

--- _app/conexao.php
+++ _app/conexao.php
@@ -2,11 +2,10 @@
 
 try
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=tcc;charset=utf8', 'root', '');
+    $pdo = new PDO('mysql:netcase.mysql.database.azure.com;dbname=tcc;charset=utf8', 'netcase', 'Tcc@2023');
 }
 catch(PDOException $e)
 {
-    echo $e->getMessage() . "</p>";
     die("Não foi possível estabelecer a conexão com o banco de dados.");
 }
 
