b09ed068e60cd7585f6d9fa6ae9c4a5b721c3029
AngeloOliveiraTech <angelodeoliveira123@gmail.com>
2023-09-21 15:12:10 -0300

    TryBD3

--- _app/conexao.php
+++ _app/conexao.php
@@ -4,15 +4,14 @@ try
 {
     $dsn = 'mysql:dbname=tcc; host=netcase.mysql.database.azure.com; charset=utf8';
     $user = 'netcase';
-    $password = 'Tcc@202';
+    $password = 'Tcc@2023';
     
     $dbh = new PDO($dsn, $user, $password);
 
 }
 catch(PDOException $e)
 {
-    echo $e;
+    echo "Erro na conexão: " . $e->getMessage();
     die("Não foi possível estabelecer a conexão com o banco de dados.");
 }
-
 
